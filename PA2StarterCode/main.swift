33496a4d4062179872f1c2060e26846f23df7256
Gina Sprint <sprint@gonzaga.edu>
2020-09-21 11:39:33 -0700

    Initial Commit

---
+++ PA2StarterCode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PA2StarterCode
+//
+//  Created by Gina Sprint on 9/21/20.
+//  Copyright © 2020 Gina Sprint. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
